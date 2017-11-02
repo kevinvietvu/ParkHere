f687d02257b5336c26b7190e8421f5e80cdcf7d7
Kevin Vu <kevinvietvu@yahoo.com>
2017-11-01 18:05:26 -0700

    the TRUE merged Kevin, Ricky and Stanley's code

---
+++ app/src/main/java/com/parkhere/android/ConfirmationOfCreationActivity.java
@@ -0,0 +1,28 @@
+package com.parkhere.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ConfirmationOfCreationActivity extends AppCompatActivity {
+
+    Button okay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_of_creation);
+
+        okay = (Button) findViewById(R.id.okay_button);
+
+        okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfirmationOfCreationActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
