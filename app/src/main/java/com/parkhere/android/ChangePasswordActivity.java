b7e36b003d774c0f676bb79cd46eb605de85ff56
Ricky Reyes <richard.reyes@sjsu.edu>
2017-12-05 02:47:44 -0800

    Change Password Update

--- app/src/main/java/com/parkhere/android/ChangePasswordActivity.java
+++ app/src/main/java/com/parkhere/android/ChangePasswordActivity.java
@@ -1,9 +1,11 @@
 package com.parkhere.android;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,21 +20,39 @@ public class ChangePasswordActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private Button saveButton;
 
+    private EditText editInputPassword;
+    private EditText editInputConfirmPassword;
+    private TextInputLayout  editInputLayoutPassword;
+    private TextInputLayout  editInputLayoutConfirmPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
-        saveButton = (Button) findViewById(R.id.save_changes);
-        passwordInput = findViewById(R.id.edit_confirm_password);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
+
+        editInputLayoutPassword = (TextInputLayout) findViewById(R.id.edit_input_layout_new_password);
+        editInputLayoutConfirmPassword = (TextInputLayout) findViewById(R.id.edit_input_layout_confirm_password);
+
+
+        editInputPassword = (EditText) findViewById(R.id.edit_input_new_password);
+        editInputConfirmPassword = (EditText) findViewById(R.id.edit_input_confirm_password);
+
+        saveButton = (Button) findViewById(R.id.save_changes);
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String password = editInputPassword.getText().toString().trim();
 
+                if(!confirmPassword()) {
+                    return;
+                }
+                editInputLayoutPassword.setErrorEnabled(false);
                 //Add password verification before uncommenting  the database call
                 //database call
                 //user.updateEmail(passwordInput.getText().toString());
@@ -40,5 +60,23 @@ public class ChangePasswordActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+    private boolean confirmPassword() {
+        String password = editInputPassword.getText().toString().trim();
+        String confirmPassword = editInputConfirmPassword.getText().toString().trim();
+        if (confirmPassword.isEmpty() || !password.equals(confirmPassword)) {
+            editInputLayoutConfirmPassword.setError(getString(R.string.err_msg_confirm_password));
+            editInputConfirmPassword.setError(getString(R.string.err_msg_required));
+            requestFocus(editInputConfirmPassword);
+            return false;
+        }
+        editInputLayoutConfirmPassword.setErrorEnabled(false);
+        return true;
+    }
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
     }
 }
